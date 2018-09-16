c14aa991a1fad9d44b25da19b2cf4e1f9ad7c608
jculumber <jennr2@aol.com>
2018-09-16 02:46:29 -0400

    added code - not functioning

--- server.js
+++ server.js
@@ -1,28 +1,37 @@
+// Require dependencies
 var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
 var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
 
-// Scraping tools
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-// Require all models
-var db = require("./models");
-
-var PORT = 3000;
+// Set up port to be either localhost's designated port or 3000
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
 
-// Configure Middleware
+// Set up Express Router
+var router = express.Router();
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
-// Use express.static to serve the public folder as a static directory
-app.use(express.static("public"));
+// Require routes files and pass router object
+require("./config/routes")(router);
+
+// Designate public folder as a static directory
+app.use(express.static(__dirname + "/public"));
+
+// Connect Handlebars to Express app
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// Use bodyParser in app
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Every request goes through router middleware
+app.use(router);
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
@@ -32,86 +41,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-// ROUTES
-
-// A GET route for scraping the website
-app.get("/scrape", function (req, res) {
-    // First we grab the body of the html with request
-    axios.get("https://www.mlb.com/cubs/news/").then(function (response) {
-        // Then we load that into cheerio and save it to $ for a shorthand selector
-        var $ = cheerio.load(response.data);
-
-        // Now we grab every div within an article tag, and do the following:
-        $("article div.article-item__bottom").each(function (i, element) {
-            // Save an empty result object
-            var result = {};
-
-            // Add the text and href of every link, and save them as properties of the result object
-            result.title = $(this)
-                .find("h1")
-                .text();
-            result.link = $(this)
-                .children("a")
-                .attr("href");
-
-            // Create a new Article using the "result" object built from scraping
-            db.Article.create(result)
-                .then(function (dbArticle) {
-                    // View the added result in the console
-                    console.log(dbArticle);
-                })
-                .catch(function (err) {
-                    // If an error occurred, send it to the client
-                    return res.json(err);
-                });
-        }).catch(function (err) {
-            // If an error occurred, send it to the client
-            return res.json(err);
-        });
-
-        // If we were able to successfully scrape and save an Article, send a message to the console
-        console.log("Scrape Complete");
-    });
-});
-
-// Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-    db.Article.find({})
-        .then(function(dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
-});
-
-// Route for grabbing a specific Article by id, populate it with its note
-app.get("/articles/:id", function(req, res) {
-    db.Article.findOne({_id: req.params.id})
-        .populate("note")
-        .then(function(dbNote) {
-            res.json(dbNote);
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
-});
-
-// Route for saving/updating and Article's associated Note
-app.post("articles/:id", function(req, res) {
-    db.Note.create(req.body)
-        .then(function(dbNote) {
-            return db.Article.findOneAndUpdate({_id: req.params.id}, {note: dbNote_id}, {new: true})
-        })
-        .then(function(dbArticle) {
-            res.json(dbArticle)
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
-});
-
-// Start the server
+// Listen on the port
 app.listen(PORT, function() {
-    console.log("App is running on " + PORT + "!");
-});
+    console.log("Listening on port:" + PORT);
+})
